7b15fb4387d10f85a693fa5c1ea66baf5ef84fff
BruceLee <brucelee@castec.com.tw>
2016-09-21 15:19:40 +0800

    modify button to show hello world

--- test/Form1.vb
+++ test/Form1.vb
@@ -1,6 +1,8 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("Hello world")
+        Dim str1 As String = "Hello"
+        Dim str2 As String = "world"
+        MsgBox(str1 + " " + str2)
     End Sub
 End Class
